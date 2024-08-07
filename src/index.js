3541f4b16a0315cc93ec752e2195c03cd0e064a1
Betojr04 <betomasia12@gmail.com>
2024-08-07 09:42:15 -0700

    added store funtionality to the template and a clickable message on the hero section to make sure the state was working when visiting the template

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Router } from "./views/Router.js";
+import { AppProvider } from "./store/AppContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router />
+    <AppProvider>
+      <Router />
+    </AppProvider>
   </React.StrictMode>
 );
