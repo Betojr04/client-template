03dcbd773e9f8141e6efc9b56170ceef4d27bf49
Betojr04 <betomasia12@gmail.com>
2024-08-07 09:17:37 -0700

    created react template to start client projects faster. just the hero section nav and footer router file setup removal of all assets from the template

---
+++ src/components/HeroSection/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Hero.css";
+
+export const Hero = () => {
+  return (
+    <section className="hero-container">
+      This is the hero section for your company website.
+    </section>
+  );
+};
